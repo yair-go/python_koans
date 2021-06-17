cfeb45c0d4ed5fd2b161f51aed850f0d2faa4f0f
yair-go <irigo1@gmail.com>
2021-06-17 09:25:12 +0300

    Triangle Project 2

--- koans/triangle.py
+++ koans/triangle.py
@@ -17,6 +17,11 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
+    if a <= 0 or b <= 0 or c <= 0:
+        raise TriangleError("All sides should be greater than 0")
+    if a + b < c or a + c < b or b + c < a:
+        raise TriangleError("The sum of any two sides should be greater than the third one")
+
     if a == b and b == c:
         return 'equilateral'
     elif a == b or b == c or a == c:
